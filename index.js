77c772ef446627ba5ffbddf1ac61b709f738c4dd
Ken <example@gmail.com>
2022-07-10 14:34:21 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+console.log(4/0);
+
+
+let somet;
+console.log(somet);
+
+const person = {
+    name: "John",
+    age: 34,
+    isMarried: false,
+}
+
+console.log(person.birthday);
+
+let arr = ['plum.png', 'gerri.jpg', 'apple.bmp'];
+
+console.log(arr[2]);
+
+/* alert('Hello Girl!'); */
+// let answer = confirm('Are you here?');
+// console.log( answer );
+
+/* let answer = prompt('Are you 18?', 'pehebs');
+console.log( typeof(arr) ); */
+
+let incr = 10,
+    derc = 10;
+
+incr++;
+derc--;
+
+console.log(incr);
+console.log(derc);
